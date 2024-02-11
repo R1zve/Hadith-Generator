c23364b3fac707390c1e302ba77cc499deddae57
Rizve <99558260+R1zve@users.noreply.github.com>
2024-02-10 20:57:19 -0500

    Update scraper.m.js

--- src/scraper.m.js
+++ src/scraper.m.js
@@ -1,5 +1,5 @@
-import axios from "src/pkg/axios.v1.6.7.js";
-import * as cheerio from "src/pkg/cheerio.v1.0.0-rc.12.js";
+import axios from "../src/pkg/axios.v1.6.7.js";
+import * as cheerio from "../src/pkg/cheerio.v1.0.0-rc.12.js";
 export async function scrape(url) {
   const response = await axios.get("https://corsproxy.io/?" + url);
   const selector = cheerio.load(response.data);
